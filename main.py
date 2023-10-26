f7d2aea3a7400f123de28070cd39cb308aff352d
Tajman <43280524+Tajman@users.noreply.github.com>
2023-10-26 20:33:05 +0000

    testing beautifulsoup and what it can pull from site.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+
+
+url = 'https://mtgdecks.net/Commander/nekusar-the-mindrazer-decklist-by-ishita-junpei-1771025'
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'html') 
+
+table = soup.find('table')
+
+print(table)
